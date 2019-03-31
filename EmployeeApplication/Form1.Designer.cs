06c7fed5cd22753e0381363b39b888c5d404a90b
Raval Ramkhelawan <ravalramkhelawan@outlook.com>
2019-04-01 01:28:11 +0200

    Initial commit

---
+++ EmployeeApplication/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace EmployeeApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.empLable = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblLast = new System.Windows.Forms.Label();
+            this.txtLast = new System.Windows.Forms.TextBox();
+            this.Email = new System.Windows.Forms.Label();
+            this.txtMail = new System.Windows.Forms.TextBox();
+            this.Phone = new System.Windows.Forms.Label();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.btnDel = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.EmpId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nam = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.LName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Mail = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ePhone = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // empLable
+            // 
+            this.empLable.AutoSize = true;
+            this.empLable.Location = new System.Drawing.Point(12, 9);
+            this.empLable.Name = "empLable";
+            this.empLable.Size = new System.Drawing.Size(95, 13);
+            this.empLable.TabIndex = 0;
+            this.empLable.Text = "Current Employees";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.EmpId,
+            this.Nam,
+            this.LName,
+            this.Mail,
+            this.ePhone});
+            this.dataGridView1.Location = new System.Drawing.Point(15, 26);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(519, 383);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.DoubleClick += new System.EventHandler(this.dataGridView1_DoubleClick);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(15, 415);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(604, 105);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(184, 20);
+            this.txtName.TabIndex = 3;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(563, 108);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(35, 13);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Name";
+            // 
+            // lblLast
+            // 
+            this.lblLast.AutoSize = true;
+            this.lblLast.Location = new System.Drawing.Point(540, 134);
+            this.lblLast.Name = "lblLast";
+            this.lblLast.Size = new System.Drawing.Size(58, 13);
+            this.lblLast.TabIndex = 0;
+            this.lblLast.Text = "Last Name";
+            // 
+            // txtLast
+            // 
+            this.txtLast.Location = new System.Drawing.Point(604, 131);
+            this.txtLast.Name = "txtLast";
+            this.txtLast.Size = new System.Drawing.Size(184, 20);
+            this.txtLast.TabIndex = 3;
+            // 
+            // Email
+            // 
+            this.Email.AutoSize = true;
+            this.Email.Location = new System.Drawing.Point(563, 160);
+            this.Email.Name = "Email";
+            this.Email.Size = new System.Drawing.Size(32, 13);
+            this.Email.TabIndex = 0;
+            this.Email.Text = "Email";
+            // 
+            // txtMail
+            // 
+            this.txtMail.Location = new System.Drawing.Point(604, 157);
+            this.txtMail.Name = "txtMail";
+            this.txtMail.Size = new System.Drawing.Size(184, 20);
+            this.txtMail.TabIndex = 3;
+            // 
+            // Phone
+            // 
+            this.Phone.AutoSize = true;
+            this.Phone.Location = new System.Drawing.Point(563, 186);
+            this.Phone.Name = "Phone";
+            this.Phone.Size = new System.Drawing.Size(38, 13);
+            this.Phone.TabIndex = 0;
+            this.Phone.Text = "Phone";
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(604, 183);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(184, 20);
+            this.txtPhone.TabIndex = 3;
+            // 
+            // btnDel
+            // 
+            this.btnDel.Location = new System.Drawing.Point(96, 415);
+            this.btnDel.Name = "btnDel";
+            this.btnDel.Size = new System.Drawing.Size(75, 23);
+            this.btnDel.TabIndex = 2;
+            this.btnDel.Text = "Delete";
+            this.btnDel.UseVisualStyleBackColor = true;
+            this.btnDel.Click += new System.EventHandler(this.btnDel_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(177, 415);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // EmpId
+            // 
+            this.EmpId.DataPropertyName = "EmployeeId";
+            this.EmpId.HeaderText = "Employee ID";
+            this.EmpId.Name = "EmpId";
+            this.EmpId.ReadOnly = true;
+            this.EmpId.Visible = false;
+            // 
+            // Nam
+            // 
+            this.Nam.DataPropertyName = "Name";
+            this.Nam.HeaderText = "Name";
+            this.Nam.Name = "Nam";
+            this.Nam.ReadOnly = true;
+            // 
+            // LName
+            // 
+            this.LName.DataPropertyName = "LastName";
+            this.LName.HeaderText = "Last Name";
+            this.LName.Name = "LName";
+            this.LName.ReadOnly = true;
+            // 
+            // Mail
+            // 
+            this.Mail.DataPropertyName = "Email";
+            this.Mail.HeaderText = "Email";
+            this.Mail.Name = "Mail";
+            this.Mail.ReadOnly = true;
+            // 
+            // ePhone
+            // 
+            this.ePhone.DataPropertyName = "Phone";
+            this.ePhone.HeaderText = "Phone";
+            this.ePhone.Name = "ePhone";
+            this.ePhone.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtMail);
+            this.Controls.Add(this.txtLast);
+            this.Controls.Add(this.Phone);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.Email);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnDel);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.lblLast);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.empLable);
+            this.Name = "Form1";
+            this.Text = "EmployeeApp";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label empLable;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblLast;
+        private System.Windows.Forms.TextBox txtLast;
+        private System.Windows.Forms.Label Email;
+        private System.Windows.Forms.TextBox txtMail;
+        private System.Windows.Forms.Label Phone;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.Button btnDel;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.DataGridViewTextBoxColumn EmpId;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nam;
+        private System.Windows.Forms.DataGridViewTextBoxColumn LName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Mail;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ePhone;
+    }
+}
+
---
+++ EmployeeApplication/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Entity;
+
+namespace EmployeeApplication
+{
+    public partial class Form1 : Form
+    {
+        Table model = new Table();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        void Clear()
+        {
+            txtName.Text = txtLast.Text = txtMail.Text = txtPhone.Text = "";
+            btnSave.Text = "Save";
+            btnDel.Enabled = false;
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Clear();
+            PopulateDataGridView();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            model.Name = txtName.Text.Trim();
+            model.LastName = txtLast.Text.Trim();
+            model.Email = txtMail.Text.Trim();
+            model.Phone = Convert.ToInt32(txtPhone.Text.Trim());
+
+            using (DBEntities db = new DBEntities())
+            {
+
+                if (model.EmployeeId == 0) //insert
+                    db.Tables.Add(model);
+                else //Update
+                    db.Entry(model).State = EntityState.Modified;
+                db.SaveChanges();
+            }
+
+            Clear();
+            PopulateDataGridView();
+            MessageBox.Show("Submitted to DB");
+        }
+
+        void PopulateDataGridView()
+        {
+            dataGridView1.AutoGenerateColumns = false;
+            using (DBEntities db = new DBEntities())
+            {
+                dataGridView1.DataSource = db.Tables.ToList<Table>();
+            }
+
+        }
+
+        private void dataGridView1_DoubleClick(object sender, EventArgs e)
+        {
+            if(dataGridView1.CurrentRow.Index != -1)
+            {
+                model.EmployeeId = Convert.ToInt32(dataGridView1.CurrentRow.Cells["EmpId"].Value);
+                using (DBEntities db = new DBEntities())
+                {
+                    model = db.Tables.Where(x => x.EmployeeId == model.EmployeeId).FirstOrDefault();
+                    txtName.Text = model.Name;
+                    txtLast.Text = model.LastName;
+                    txtMail.Text = model.Email;
+                    txtPhone.Text = Convert.ToString(model.Phone);
+                              
+                }
+                btnSave.Text = "Update";
+                btnDel.Enabled = true;
+
+            }
+        }
+
+        private void btnDel_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Are you sure you wish to delete this record??", "Delete Operation", MessageBoxButtons.YesNo) == DialogResult.Yes )
+            {
+                using (DBEntities db = new DBEntities())
+                {
+                    var entry = db.Entry(model);
+                    if (entry.State == EntityState.Detached)
+                        db.Tables.Attach(model);
+                    db.Tables.Remove(model);
+                    db.SaveChanges();
+                    PopulateDataGridView();
+                    Clear();
+
+                    MessageBox.Show("Deleted Successfully");
+                }
+            }
+        }
+    }
+}
